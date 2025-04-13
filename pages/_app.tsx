3e17352d24f68733ce29198259ab729d95f5c6d1
rohansingh2612 <rohansingh.cyberking@gmail.com>
2025-04-13 20:37:03 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1571996475837550"
-     crossorigin="anonymous"></script>
-        {/* Google AdSense Script */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          crossOrigin="anonymous"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: "ca-pub-1571996475837550", // Replace with your AdSense publisher ID
-                enable_page_level_ads: true
-              });
-            `,
-          }}
-        ></script>
+        {/* Google AdSense Meta Tag */}
+        <meta name="google-adsense-account" content="ca-pub-1571996475837550" />
       </Head>
       <Component {...pageProps} />
     </>
