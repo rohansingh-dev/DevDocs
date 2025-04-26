fcd7b602d15571dc1393605c381ecf5703fa5cb4
rohansingh-dev <rohansingh.cyberking@gmail.com>
2025-04-26 22:22:25 +0530

    up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,6 +81,13 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
